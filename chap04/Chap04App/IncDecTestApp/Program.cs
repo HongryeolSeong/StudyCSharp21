1ad2e62a13e74101dff91ba9e66f7fd65a04f52b
ghd0276 <ghd0276@naver.com>
2021-02-22 17:27:21 +0900

    2/22 4장 전치 후치

---
+++ chap04/Chap04App/IncDecTestApp/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace IncDecTestApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("증감연산자 테스트");
+
+            int result = 26;
+            Console.WriteLine($"현재 숫자는 {result}");
+            result += 3;
+            Console.WriteLine($"현재 숫자는 {result}");
+            result -= 10;
+            Console.WriteLine($"현재 숫자는 {result}");
+            Console.WriteLine($"전치 증가 값은 {++result}"); // 전위 증감
+            Console.WriteLine($"후치 증가 값은 {result++}"); // 후위 증감 -> 이 실행문이 끝나고 1 증가함
+            Console.WriteLine($"전치 감소 값은 {--result}"); // 실행전 1 감소
+            Console.WriteLine($"후치 감소 값은 {result--}"); // 실행후 1 감소
+            Console.WriteLine($"최종 값은 {result}");
+
+        }
+    }
+}
