17b364227863b1fd42b8fa5db910fef80e51c8b8
ghd0276 <ghd0276@naver.com>
2021-02-26 14:01:09 +0900

    2/26 7장 마무리

---
+++ chap07/Chap07App/TupleTestApp/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TupleTestApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var tuple1 = (1, "성홍렬", "010-7759-0276", "부산시 금정구", true);
+            var tuple2 = (Idx: 2, Name: "홍길동", Phone: "010-9999-9999", Address: "경남 창원시 의창구", Marrige: false);
+
+            Console.WriteLine($"{tuple1.Item1} / {tuple1.Item2} / {tuple1.Item3} / {tuple1.Item4} / {tuple1.Item5}");
+            Console.WriteLine($"{tuple2.Item1} / {tuple2.Item2} / {tuple2.Item3} / {tuple2.Item4} / {tuple2.Item5}");
+            Console.WriteLine($"{tuple2.Idx} / {tuple2.Name} / {tuple2.Phone} / {tuple2.Address} / {tuple2.Marrige}");
+        }
+    }
+}
