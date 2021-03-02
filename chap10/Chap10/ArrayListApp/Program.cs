99b8c381701b67de68526a54e33de14ecb24339e
ghd0276 <ghd0276@naver.com>
2021-03-02 10:34:38 +0900

    3/2 10장 완료

---
+++ chap10/Chap10/ArrayListApp/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayListApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // ArrayList
+            ArrayList array = new ArrayList(new[] { 80, 74, 81, 90, 34 });
+            Console.WriteLine("81위치에 50추가");
+            var loc = array.IndexOf(81);
+            array.Insert(loc, 50);
+
+            foreach (var item in array)
+            {
+                Console.WriteLine($" {item}");
+
+            }
+
+            // 90 지우기
+            Console.WriteLine("90삭제");
+            array.RemoveAt(4);
+            foreach (var item in array)
+            {
+                Console.WriteLine($" {item}");
+
+            }
+
+            Console.WriteLine("정렬");
+            array.Sort();
+            foreach (var item in array)
+            {
+                Console.WriteLine($" {item}");
+
+            }
+        }
+    }
+}
---
+++ chap10/Chap10/HashTableApp/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+
+namespace HashTableApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("HashTalbe 예제");
+
+            Hashtable ht = new Hashtable();
+            ht["일"] = "one";
+            ht["이"] = "two";
+            ht["삼"] = "three";
+            ht["사"] = "four";
+
+            Console.WriteLine(ht["일"]);
+            Console.WriteLine(ht["이"]);
+            Console.WriteLine(ht["삼"]);
+            Console.WriteLine(ht["사"]);
+
+            foreach (DictionaryEntry item in ht)
+            {
+                Console.WriteLine($"{item.Key} : {item.Value}"); // 순서가 랜덤 -> 키값이 있기때문에 정렬 x
+            }
+
+            // 여러가지 초기화 방법
+            int[] arr = { 123, 456, 789 };
+            ArrayList list = new ArrayList(arr);
+            ArrayList List2 = new ArrayList(new int[] { 123, 456, 789 });
+            ArrayList List3 = new ArrayList() { 123, 456, 789 };
+        }
+    }
+}
