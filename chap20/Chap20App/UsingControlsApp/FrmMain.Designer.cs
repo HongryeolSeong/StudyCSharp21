9f05d784043af40a23e7c5deb1773610d4fe9227
ghd0276 <ghd0276@naver.com>
2021-03-05 16:53:10 +0900

    3/5 20장 윈폼 트랙바 완료

---
+++ chap20/Chap20App/UsingControlsApp/FrmMain.Designer.cs
@@ -0,0 +1,171 @@
+﻿
+namespace UsingControlsApp
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmMain));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.TxtResult = new System.Windows.Forms.TextBox();
+            this.ChkItalic = new System.Windows.Forms.CheckBox();
+            this.CboFonts = new System.Windows.Forms.ComboBox();
+            this.ChkBold = new System.Windows.Forms.CheckBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.TrbHandle = new System.Windows.Forms.TrackBar();
+            this.PrbDisplay = new System.Windows.Forms.ProgressBar();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.TrbHandle)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.TxtResult);
+            this.groupBox1.Controls.Add(this.ChkItalic);
+            this.groupBox1.Controls.Add(this.CboFonts);
+            this.groupBox1.Controls.Add(this.ChkBold);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(384, 104);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "ComboBox, CheckBox, TextBox";
+            // 
+            // TxtResult
+            // 
+            this.TxtResult.Location = new System.Drawing.Point(17, 67);
+            this.TxtResult.Name = "TxtResult";
+            this.TxtResult.Size = new System.Drawing.Size(348, 21);
+            this.TxtResult.TabIndex = 8;
+            // 
+            // ChkItalic
+            // 
+            this.ChkItalic.AutoSize = true;
+            this.ChkItalic.Font = new System.Drawing.Font("굴림", 9F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.ChkItalic.Location = new System.Drawing.Point(305, 31);
+            this.ChkItalic.Name = "ChkItalic";
+            this.ChkItalic.Size = new System.Drawing.Size(60, 16);
+            this.ChkItalic.TabIndex = 7;
+            this.ChkItalic.Text = "이탤릭";
+            this.ChkItalic.UseVisualStyleBackColor = true;
+            this.ChkItalic.CheckedChanged += new System.EventHandler(this.ChkItalic_CheckedChanged);
+            // 
+            // CboFonts
+            // 
+            this.CboFonts.FormattingEnabled = true;
+            this.CboFonts.Location = new System.Drawing.Point(62, 29);
+            this.CboFonts.Name = "CboFonts";
+            this.CboFonts.Size = new System.Drawing.Size(183, 20);
+            this.CboFonts.TabIndex = 6;
+            this.CboFonts.SelectedIndexChanged += new System.EventHandler(this.CboFonts_SelectedIndexChanged);
+            // 
+            // ChkBold
+            // 
+            this.ChkBold.AutoSize = true;
+            this.ChkBold.Font = new System.Drawing.Font("굴림", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.ChkBold.Location = new System.Drawing.Point(251, 31);
+            this.ChkBold.Name = "ChkBold";
+            this.ChkBold.Size = new System.Drawing.Size(50, 16);
+            this.ChkBold.TabIndex = 5;
+            this.ChkBold.Text = "굵게";
+            this.ChkBold.UseVisualStyleBackColor = true;
+            this.ChkBold.CheckedChanged += new System.EventHandler(this.ChkBold_CheckedChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(15, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 12);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Font : ";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.PrbDisplay);
+            this.groupBox2.Controls.Add(this.TrbHandle);
+            this.groupBox2.Location = new System.Drawing.Point(12, 122);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(384, 122);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "TrackBar & ProgressBar";
+            // 
+            // TrbHandle
+            // 
+            this.TrbHandle.Location = new System.Drawing.Point(17, 35);
+            this.TrbHandle.Maximum = 20;
+            this.TrbHandle.Name = "TrbHandle";
+            this.TrbHandle.Size = new System.Drawing.Size(348, 45);
+            this.TrbHandle.TabIndex = 0;
+            this.TrbHandle.Scroll += new System.EventHandler(this.TrbHandle_Scroll);
+            // 
+            // PrbDisplay
+            // 
+            this.PrbDisplay.Location = new System.Drawing.Point(17, 86);
+            this.PrbDisplay.Maximum = 20;
+            this.PrbDisplay.Name = "PrbDisplay";
+            this.PrbDisplay.Size = new System.Drawing.Size(348, 23);
+            this.PrbDisplay.TabIndex = 1;
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(408, 386);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "FrmMain";
+            this.Text = "Using Control App";
+            this.Load += new System.EventHandler(this.FrmMain_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.TrbHandle)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox TxtResult;
+        private System.Windows.Forms.CheckBox ChkItalic;
+        private System.Windows.Forms.ComboBox CboFonts;
+        private System.Windows.Forms.CheckBox ChkBold;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.ProgressBar PrbDisplay;
+        private System.Windows.Forms.TrackBar TrbHandle;
+    }
+}
+
---
+++ chap20/Chap20App/UsingControlsApp/FrmMain.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UsingControlsApp
+{
+    public partial class FrmMain : Form
+    {
+        public FrmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void FrmMain_Load(object sender, EventArgs e)
+        {
+            var FontsList = FontFamily.Families;
+            foreach (var font in FontsList)
+            {
+                CboFonts.Items.Add(font.Name);
+            }
+        }
+        /// <summary>
+        /// 콤보박스, 체크박스 값 변경으로 텍스트 값 폰트 바꾸기
+        /// </summary>
+        private void ChangeFont()
+        {
+            // if (CboFonts.SelectedIndex < 0) return; // 콤보박스에 아무것도 선택안된경우(-1) 메소드 탈출
+
+            FontStyle style = FontStyle.Regular;
+            if (ChkBold.Checked) style |= FontStyle.Bold; // 00000001
+            if (ChkItalic.Checked) style |= FontStyle.Italic; // 00000010
+            // 00000001 | 00000010 = 00000011 => Bold + Italic
+
+            TxtResult.Font = new Font((string)CboFonts.SelectedItem, 14, style);
+        }
+
+        private void CboFonts_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            ChangeFont();
+        }
+
+        private void ChkBold_CheckedChanged(object sender, EventArgs e)
+        {
+            ChangeFont();
+        }
+
+        private void ChkItalic_CheckedChanged(object sender, EventArgs e)
+        {
+            ChangeFont();
+        }
+
+        private void TrbHandle_Scroll(object sender, EventArgs e)
+        {
+            PrbDisplay.Value = TrbHandle.Value;
+        }
+    }
+}
