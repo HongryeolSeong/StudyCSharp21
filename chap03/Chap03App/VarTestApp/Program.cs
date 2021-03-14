395b93ceb6d68d1cad561a60165cd2da02a7c239
ghd0276 <ghd0276@naver.com>
2021-03-14 19:16:37 +0900

    3/14 타이틀 수정

--- chap03/Chap03App/VarTestApp/Program.cs
+++ chap03/Chap03App/VarTestApp/Program.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace VarTestAo
+namespace VarTestApp
 {
     class Program
     {
@@ -12,6 +12,6 @@ namespace VarTestAo
         {
             var i = 100.5; // var는 지역변수만 가능
             Console.WriteLine($"정수값은 {i}입니다.");
-        }
+        }r
     }
 }
