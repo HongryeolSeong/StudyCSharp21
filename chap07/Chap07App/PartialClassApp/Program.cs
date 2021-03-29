26648bc24cf03bbbadf0f877ce8571e062470a17
ghd0276 <ghd0276@naver.com>
2021-03-29 22:03:36 +0900

    3/29 복습

--- chap07/Chap07App/PartialClassApp/Program.cs
+++ chap07/Chap07App/PartialClassApp/Program.cs
@@ -9,7 +9,6 @@ namespace PartialClassApp
     partial class MyClass
     {
         public void Method3() { Console.WriteLine("Method3"); }
-
         public void Method4() { Console.WriteLine("Method4"); }
     }
     class Program
